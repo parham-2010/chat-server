3f191634794b88d69e398306e5804b601eca59ac
parham-2010 <parhamdashti4490@gmail.com>
2025-07-13 18:36:09 +0400

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-messages = []  # ذخیره پیام‌ها موقتاً در حافظه
+messages = []
 
 @app.route('/send', methods=['POST'])
 def send_message():
     data = request.json
     messages.append(data)
-    return jsonify({"status": "received"}), 200
+    return jsonify({"status": "received"})
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    return jsonify(messages), 200
+    return jsonify(messages)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host='0.0.0.0', port=5000)
