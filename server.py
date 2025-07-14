25e61769625bc31fa196b8e2f3eea2096f9e3c96
parham-2010 <parhamdashti4490@gmail.com>
2025-07-14 21:27:40 +0400

    Update server.py

--- server.py
+++ server.py
@@ -27,5 +27,36 @@ def send_message():
 def get_messages():
     return jsonify(messages)
 
+
+@app.route('/delete_msg', methods=['POST'])
+def delete_message():
+    data = request.json
+    user = data.get("user")
+    message_to_delete = data.get("message")
+
+    if user in messages:
+        try:
+            messages[user].remove(message_to_delete)
+            return jsonify({"status": "message deleted", "user": user})
+        except ValueError:
+            return jsonify({"status": "message not found"}), 404
+    else:
+        return jsonify({"status": "user not found"}), 404
+
+
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    data = request.json
+    user = data.get("user")
+
+    if user in messages:
+        del messages[user]
+        return jsonify({"status": "deleted", "user": user})
+    else:
+        return jsonify({"status": "not found", "user": user}), 404
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
